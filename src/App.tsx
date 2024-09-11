ad821d6ae6a28961dee995380875b4bff490bd3b
Zeizvant <oxanashvilizezva@gmail.com>
2024-09-11 20:16:30 +0400

    feat/add navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Navbar} from "./components/Navbar/Navbar.tsx";
 
 function App() {
 
   return (
     <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element=''/>
+        </Routes>
+      </Router>
     </>
   )
 }
